e02d401f1387f7602fc73f35904a7708508b8e2e
Kristaps-M <kristapsmitins@gmail.com>
2022-12-07 18:30:59 +0200

    compare id's and print all 4

--- ycombinator_scraper2.py
+++ ycombinator_scraper2.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get("https://news.ycombinator.com/")
+response = requests.get("https://news.ycombinator.com/?p=3")
 if response.status_code != 200:
 	print("Error fetching page")
 	exit()
@@ -11,23 +11,60 @@ else:
 
 soup = BeautifulSoup(response.content, 'html.parser')
 
+def test_yolo(AAA, BBB):
+	if(AAA.get("id") == BBB.span.get("id").split("score_")[1]):
+		print("YES ----------- Id's are the same")
+		print(str(first.find(class_="titleline").a.get("href")).replace("\n","")) # .get("href") .replace("\n","")
+		print(str(first.find(class_="titleline").get_text()).replace("\n",""))
+		print(first_b.span.get_text())
+		print(first_b.find(class_="age").get("title"))
+	else:
+		print("NO")
+
 all_at_athing = [a for a in soup.find_all(class_="athing")]
 all_at_subline = [a for a in soup.find_all(class_="subline")]
 all_at_things = [a for a in soup.find_all(class_="titleline")]  #  "athing"
 all_subtext = [a for a in soup.find_all(class_="subtext")]
 print(len(all_at_athing), len(all_at_subline), len(all_at_things), len(all_subtext))
 
-a = 0
-for i in all_at_athing:
-	print(i.get("id"))
-	a+=1
-print(a)
 
-b=0
-for i in all_at_subline:
-	print(i.span.get("id")) # .find("id")
-	b+=1
-print(b)
+
+# for a,b in zip(all_at_athing, all_at_subline):
+# 	test_yolo(a, b)
+# 	print("-------------- end --------")
+first = all_at_athing[0]
+first_b = all_at_subline[0]
+#(title, link, points, date created)
+print(first)
+
+print(first_b)
+
+print(first.get("id"))
+print(first_b.span.get("id"))
+if(first.get("id") == first_b.span.get("id").split("score_")[1]):
+	print("YES ----------- Id's are the same")
+	print(str(first.find(class_="titleline").a.get("href")).replace("\n","")) # .get("href") .replace("\n","")
+	print(str(first.find(class_="titleline").get_text()).replace("\n",""))
+	print(first_b.span.get_text())
+	print(first_b.find(class_="age").get("title"))
+else:
+	print("NO")
+
+
+
+# a = 0
+# for i in all_at_athing:
+# 	print(i.get("id"))
+# 	a+=1
+# print(a)
+
+# b=0
+# for i in all_at_subline:
+# 	print(i.span.get("id")) # .find("id")
+# 	b+=1
+# print(b)
+
+#----------------------------------------
 # age_count = 0
 # for thing in all_at_things:
 # 	#print(thing)
