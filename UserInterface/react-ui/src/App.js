64af7c803d66cf1ce4363666614da3056de7287f
Kristaps-M <kristapsmitins@gmail.com>
2022-12-09 17:01:19 +0200

    update. Add react, add boot strap, install router dom

---
+++ UserInterface/react-ui/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import {Home} from './Home';
+import { HackerData } from './HackerData';
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <h3 className = "d-flex justify-content-center m-3">
+        React JS FrontEnd
+      </h3>
+    </div>
+  );
+}
+
+export default App;
---
+++ UserInterface/react-ui/src/HackerData.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export class HackerData extends Component {
+  render (){
+    return (
+      <div>
+        <h3>This is HackerData page!</h3>
+      </div>
+    )
+  }
+};
