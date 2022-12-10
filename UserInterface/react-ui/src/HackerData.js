1e7d9f4688a183a7159117ca152bd02441f6023b
Kristaps-M <kristapsmitins@gmail.com>
2022-12-10 15:08:41 +0200

    dispays all react screen

--- UserInterface/react-ui/src/HackerData.js
+++ UserInterface/react-ui/src/HackerData.js
@@ -1,10 +1,63 @@
 import React, {Component} from 'react';
+import {variables} from './Variables.js'
 
 export class HackerData extends Component {
+
+  constructor (props) {
+    super(props);
+
+    this.state ={
+      hackerdataList:[]
+    }
+  }
+
+  refreshList(){
+    fetch(variables.API_URL+'Hackerdata')
+    .then(response => response.json())
+    .then(data => this.setState({hackerdataList:data}))
+  }
+
+  componentDidMount(){
+    this.refreshList();
+  }
+
   render (){
+    const {hackerdataList} = this.state;
     return (
       <div>
         <h3>This is HackerData page!</h3>
+        <table className='table table-striped'>
+          <thead>
+            <tr>
+              <th>
+                the_id
+              </th>
+              <th>
+                title
+              </th>
+              <th>
+                link
+              </th>
+              <th>
+                points
+              </th>
+              <th>
+                date_created
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {hackerdataList.map(item =>
+              <tr key={item.the_id}>
+                <td>{item.the_id}</td>
+                <td>{item.title}</td>
+                <td>{item.link}</td>
+                <td>{item.points}</td>
+                <td>{item.date_created}</td>                
+              </tr>
+              )}
+          </tbody>
+        </table>
       </div>
     )
   }
